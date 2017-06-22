964e0b4659ed65e5a8ffc73626d12ccb8721d23d
k-koehler <kevin.koehler@usask.ca>
2017-06-21 21:05:35 -0600

    Implemented chopWinterface. Next up, processList. Will have to come up with a method to adjust contrast in Bitmaps and invert colours in Bitmaps.

--- EasySweeper 3/EasySweeper 3/MapOperations.cs
+++ EasySweeper 3/EasySweeper 3/MapOperations.cs
@@ -56,13 +56,64 @@ namespace EasySweeper_3 {
         }
 
         /// <summary>
+        /// crops a bitmap according to a rectangle
+        /// </summary>
+        /// <param name="img">crop this image</param>
+        /// <param name="rec">crop the image at rec.location and rec.size</param>
+        /// <returns>new bitmap (not reference) of the cropped bitmap</returns>
+        public static Bitmap cropBitmap(Bitmap img, Rectangle rec) {
+
+            if (img == null)
+                return null;
+
+            Bitmap nb = new Bitmap(rec.Width, rec.Height);
+            Graphics g = Graphics.FromImage(nb);
+            g.DrawImage(img, -rec.X, -rec.Y);
+            return nb;
+        }
+
+        /// <summary>
         /// chops the winterface into OCR-Friendly sub-bitmaps
         /// </summary>
         /// <param name="winterface">an image of the winterface</param>
         /// <returns>list of sub-bitmaps which are the useful bits to use OCR on</returns>
         private static List<Bitmap> chopWinterface(ref Bitmap winterface) {
-            //TODO
-            return null;
+
+            //make sure the winterface has the right dimensions
+            if (winterface.Width != 499 || winterface.Height != 334)
+                throw new FormatException("Invalid winterface dimensions");
+
+            //some random consts for your viewing pleasure
+            var rectangleList = new List<Rectangle> {
+                new Rectangle(new Point(35,35),   new Size(44,4)),   //timer                  -> 0
+                new Rectangle(new Point(49,79),   new Size(49,79)),  //floor number           -> 1
+                new Rectangle(new Point(307,171), new Size(37,9)),   //percentage completed   -> 2
+                new Rectangle(new Point(310,191), new Size(33,9)),   //level mod              -> 3
+                new Rectangle(new Point(358,61),  new Size(102,41)), //player 1               -> 4
+                new Rectangle(new Point(358,111), new Size(102,41)), //player 2               -> 5
+                new Rectangle(new Point(358,161), new Size(102,41)), //player 3               -> 6
+                new Rectangle(new Point(358,211), new Size(102,41)), //player 4               -> 7
+                new Rectangle(new Point(358,261), new Size(102,41))  //player 5               -> 8
+            };
+
+            //crop each rectangle from the bitmap and add it to the list
+            List<Bitmap> list = new List<Bitmap>();
+            foreach(var rec in rectangleList) {
+               list.Add(cropBitmap(winterface, rec));
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// this method will contrast and invert each bitmap in a list
+        /// </summary>
+        /// <param name="winterfaceInformation"></param>
+        private static void processList(ref List<Bitmap> winterfaceInformation) {
+            foreach(var bmp in winterfaceInformation) {
+                //TODO adjust contrast
+                //TODO invert colours
+            }
         }
 
         /// <summary>
@@ -73,6 +124,7 @@ namespace EasySweeper_3 {
         public static List<String> readWinterface(ref Bitmap winterface) {
             //chop into OCR friendly bits
             var ocr_friendly = chopWinterface(ref winterface);
+            processList(ref ocr_friendly);
             //TODO
             return null;
         }
