b02991ea75e725f08b7d1d7be578c1991c4c8815
NEDSPC\Ned <grady.ned@gmail.com>
2017-07-05 21:59:53 +0100

    Added stuff

---
+++ EasySweeper 3/EasyWinterface/Conversions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EasyWinterface
+{
+    static class Conversions
+    {
+        public static int ToFloorNumber(string floorString)
+        {
+            return JustDigits(floorString);
+        }
+
+        public static TimeSpan ToTimeSpan(string timeString, char[] delimiters = null)
+        {
+            char[] split = delimiters ?? new char[] { ':' };
+            string[] splitTimes = timeString.Split(split);
+
+                return new TimeSpan(
+                    Convert.ToInt32(splitTimes[0]),
+                    Convert.ToInt32(splitTimes[1]),
+                    Convert.ToInt32(splitTimes[2]));
+        }
+
+        public static int ToBonusPercentage(string bonusString)
+        {
+            return JustDigits(bonusString);
+        }
+
+        public static int ToLevelMod(string modString)
+        {
+            return JustDigits(modString);
+        }
+
+        public static int JustDigits(string str)
+        {               
+            return Convert.ToInt32(Regex.Replace(str, "^[0-9]+$", ""));
+        }
+    }
+}
--- EasySweeper 3/EasyWinterface/Floor.cs
+++ EasySweeper 3/EasyWinterface/Floor.cs
@@ -8,5 +8,38 @@ namespace EasyWinterface
 {
     class Floor
     {
+        private List<Player> _players;
+        private TimeSpan _time;
+        private int _floor;
+        private int _bonusPercentage;
+
+        public IEnumerable<Player> Players => _players;
+        public TimeSpan Time => _time;
+        public int FloorNum => _floor;
+        public int BonusPercentage => _bonusPercentage;
+
+        public static explicit operator Floor(List<string> strings)
+        {
+            List<Player> players = new List<Player>();
+
+            int floor = Conversions.ToFloorNumber(strings[0]);
+            TimeSpan time = Conversions.ToTimeSpan(strings[1]);
+            int bonus = Conversions.ToBonusPercentage(strings[2]);
+            int mod = Conversions.ToLevelMod(strings[3]);
+            
+
+
+            return null;
+        }
+
+        public Floor(IEnumerable<Player> players, TimeSpan time, int floor, int bonusPercentage)
+        {            
+            _players = players?.ToList<Player>() ?? new List<Player>();
+            _time = time;            
+        }
+
+
+
+
     }
 }
