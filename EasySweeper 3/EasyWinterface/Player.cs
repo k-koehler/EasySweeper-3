2b00c66cf4af27e3f15fa4499655a03b2050513f
NEDSPC\Ned <grady.ned@gmail.com>
2017-07-05 20:12:16 +0100

    Added player class

---
+++ EasySweeper 3/EasyWinterface/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWinterface
+{
+    class Player
+    {
+        private int? _id;
+        private string _user;
+
+        public int? ID => _id;
+        public string User => _user;
+
+        public Player(int? id, string user)
+        {
+            _id = id;
+            _user = user;
+        }
+
+        public Player(string name)
+            : this(null, name)
+        { }
+
+
+    }
+}
