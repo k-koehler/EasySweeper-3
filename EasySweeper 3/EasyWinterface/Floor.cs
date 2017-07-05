2ede0889ea2e99dfa0521655b0bca0e44b51c2d0
k-koehler <kevin.koehler@usask.ca>
2017-07-05 15:52:18 -0600

    asda

--- EasySweeper 3/EasyWinterface/Floor.cs
+++ EasySweeper 3/EasyWinterface/Floor.cs
@@ -12,13 +12,11 @@ namespace EasyWinterface
         private TimeSpan _time;
         private int _floor;
         private int _bonusPercentage;
-        private int _mod;
 
         public IEnumerable<Player> Players => _players;
         public TimeSpan Time => _time;
         public int FloorNum => _floor;
         public int BonusPercentage => _bonusPercentage;
-        public int Mod => _mod;
 
         public static explicit operator Floor(List<string> strings)
         {
@@ -28,38 +26,20 @@ namespace EasyWinterface
             TimeSpan time = Conversions.ToTimeSpan(strings[1]);
             int bonus = Conversions.ToBonusPercentage(strings[2]);
             int mod = Conversions.ToLevelMod(strings[3]);
+            
 
-            //strings 4 -> 8 are the player names.
-            //an empty string ("") means the OCR failed
-            //a null string means this player was a leecher, so we'll discard this player.
-            for (int i = 4; i <= 8; i++)
-            {
-                if(strings[i] != null)
-                {
-                    players.Add(new Player(strings[i]));
-                }
-            }
 
-            return new Floor(players, time, floor, bonus, mod);
+            return null;
         }
 
-        public Floor(IEnumerable<Player> players, TimeSpan time, int floor, int bonusPercentage, int mod)
+        public Floor(IEnumerable<Player> players, TimeSpan time, int floor, int bonusPercentage)
         {            
             _players = players?.ToList<Player>() ?? new List<Player>();
-            _time = time;
-            _floor = floor;
-            _bonusPercentage = bonusPercentage;
-            _mod = mod;
+            _time = time;            
         }
 
-        public override string ToString()
-        {
-            return String.Format("Floor: {0} Time: {1} Bonus: {2} Mod: {3}",
-                _floor,
-                _time.ToString(),
-                _bonusPercentage,
-                _mod);
-        }
+
+
 
     }
 }
