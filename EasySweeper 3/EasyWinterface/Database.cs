4006579e805bb238bf3ed3edda38b65b48077883
NEDSPC\Ned <grady.ned@gmail.com>
2017-07-06 23:54:08 +0100

    Added database test

---
+++ EasySweeper 3/EasyWinterface/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWinterface
+{
+    static class Database
+    {
+        public static string ConnectionString => "Server=NEDSPC\\SQLEXPRESS;Initial Catalog=EasySweeper;uid=EasySweeper;pwd=fkfut";
+
+        public static async Task<bool> Test()
+        {
+            int ret=0;
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = await Sproc("spTestConnection", conn))
+                {
+                   // Console.WriteLine("in reader");
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                           // Console.WriteLine("in reader");
+                            ret = reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+
+            return ret == 1;
+        }
+
+        private static async Task<SqlCommand> Sproc(string sprocName, SqlConnection conn)
+        {
+            await conn.OpenAsync();
+
+            return new SqlCommand()
+            {
+                CommandText = sprocName,
+                CommandType = CommandType.StoredProcedure,
+                Connection = conn
+            };
+        }
+    }
+}
---
+++ EasySweeper 3/EasyWinterface/Storage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWinterface
+{
+    class Storage
+    {
+        public async static Task<bool> AddFloor(Floor floor)
+        {
+            return false;
+        }
+
+        public async static Task<bool> TestDatabase()
+        {
+            bool success = await Database.Test();
+            Console.WriteLine(success ? "\n Yes" : "\n No");
+            return  success;
+        }
+    }
+}
